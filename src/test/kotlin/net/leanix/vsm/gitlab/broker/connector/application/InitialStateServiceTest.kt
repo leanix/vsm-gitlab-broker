c8b1200a7cd2c0b50f429d289e1c04716036a263
ahmed-ali-55 <ahmed.ali@leanix.net>
2023-09-15 11:45:14 +0200

    Adding generateDoraEvents assertion to InitState Test

--- src/test/kotlin/net/leanix/vsm/gitlab/broker/connector/application/InitialStateServiceTest.kt
+++ src/test/kotlin/net/leanix/vsm/gitlab/broker/connector/application/InitialStateServiceTest.kt
@@ -14,26 +14,31 @@ class InitialStateServiceTest {
 
     private val repositoryService = mockk<RepositoryService>()
     private val commandProvider = mockk<CommandProvider>()
-    private val initialStateService = spyk<InitialStateService>(InitialStateService(repositoryService, commandProvider))
+    private val doraService = mockk<DoraService>()
+    private val initialStateService =
+        spyk<InitialStateService>(InitialStateService(repositoryService, commandProvider, doraService))
 
     @BeforeEach
     fun setup() {
         every { initialStateService.logFailedStatus(any(), any()) } returns Unit
         every { commandProvider.sendCommand(any(), any()) } returns Unit
+        every { doraService.generateDoraEvents(any(), any()) } returns Unit
     }
 
     @Test
     fun `should send correct command for successful and failed assignments`() {
         val assignmentToSucceed = getGitlabAssignment()
         val assignmentToFail = getGitlabAssignment()
+        val repository = getRepository()
 
         every { repositoryService.importAllRepositories(assignmentToFail) } throws Exception("some error")
-        every { repositoryService.importAllRepositories(assignmentToSucceed) } returns Unit
+        every { repositoryService.importAllRepositories(assignmentToSucceed) } returns listOf(repository)
 
         initialStateService.initState(listOf(assignmentToSucceed, assignmentToFail))
 
         verifyOrder {
             repositoryService.importAllRepositories(assignmentToSucceed)
+            doraService.generateDoraEvents(repository, assignmentToSucceed)
             repositoryService.importAllRepositories(assignmentToFail)
 
             commandProvider.sendCommand(assignmentToSucceed, CommandEventAction.FINISHED)
