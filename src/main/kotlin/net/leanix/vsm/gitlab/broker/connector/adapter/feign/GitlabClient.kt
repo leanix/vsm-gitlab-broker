00e084f431b6077e1541bd413378a71c66c2271a
alfredo-mfaria <alfredo.faria88@gmail.com>
2023-11-06 15:36:47 +0100

    CID-2047 Get top level groups only

--- src/main/kotlin/net/leanix/vsm/gitlab/broker/connector/adapter/feign/GitlabClient.kt
+++ src/main/kotlin/net/leanix/vsm/gitlab/broker/connector/adapter/feign/GitlabClient.kt
@@ -6,6 +6,7 @@ import net.leanix.vsm.gitlab.broker.connector.adapter.feign.data.GitlabVersionRe
 import net.leanix.vsm.gitlab.broker.shared.auth.adapter.feign.GitlabFeignClientConfiguration
 import org.springframework.cloud.openfeign.FeignClient
 import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.RequestParam
 
 @FeignClient(
     name = "gitlab",
@@ -17,7 +18,9 @@ interface GitlabClient {
     fun getCurrentUser(): GitlabUser
 
     @GetMapping("/groups")
-    fun getAllGroups(): List<GitlabGroup>
+    fun getAllGroups(
+            @RequestParam("top_level_only") topLevelOnly: Boolean = true
+    ): List<GitlabGroup>
 
     @GetMapping("/version")
     fun getVersion(): GitlabVersionResponse
